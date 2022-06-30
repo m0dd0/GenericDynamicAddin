138ab6fc52f6f9fec8d33ad81b0c9eb07d303cde
Moritz <mo.hesche@gmail.com>
2022-06-30 14:02:26 +0200

    simplified version with lambdas

--- GenericDynamicAddin.py
+++ GenericDynamicAddin.py
@@ -13,66 +13,92 @@ from .src.ui import InputIds, CommandWindow
 command = None  # needed for custom event handlers see def on_custom_event()
 periodic_thread = None  # started in creaed handler and stoppen on destroy
 execution_queue = Queue()
+i = 0
 
 
 # handlers #####################################################################
-def thread_execute():
-    # to get fusion work done from an thread you normally fire a custom event:
-    # ao.app.fireCustomEvent(custom_event_id)
-    # however since the we are still running a command in the foreground we can not simply execute
-    # something fusion related from the custom event handler.
-    # Instead we need to call the commad.doExecute(False) method and execute the work tbd from the 
-    # active commands execute handler. 
-    # To achieve arbitrary execution we utilize a global execution queue.
-
-    # Note that sometimes it might also work to directly something from the custom event handler
-    # or call the command.doExecute(False) method directly from the thread.
-    # However the most rliable method is the one described above.
-
-    adsk.core.Application.get().fireCustomEvent("CommandActionTriggerEventId")
-    # alternativly we could use faf.utils.execute_as_event(on_custom_event)
-    # in this case the on_custom_event function must not accept any arguments and we must also ensure
-    # that the event gets created in the command created handler
-
-
-def on_custom_event(event_args: adsk.core.CustomEventArgs):
+def trigger_command_queue():
     # command cant be retrieved from args --> global instance necessary
+    global i
+
     if command.isValid:
         execution_queue.put(
-            lambda: faf.utils.create_cube((0,0,0),10)
+            lambda: adsk.core.Application.get().activeProduct.rootComponent.bRepBodies.add(
+                faf.utils.create_cube((i * 10, 0, 0), 10)
+            )
         )
         command.doExecute(False)
+        i = i + 1
+
 
 def on_created(event_args: adsk.core.CommandCreatedEventArgs):
     global command
     command = event_args.command
 
-    adsk.core.Application.get().activeDocument.design.designType = adsk.fusion.DesignTypes.DirectDesignType
+    adsk.core.Application.get().activeDocument.design.designType = (
+        adsk.fusion.DesignTypes.DirectDesignType
+    )
 
     command_window = CommandWindow(command)
 
+    # to get fusion work done from an thread you normally fire a custom event:
+    # ao.app.fireCustomEvent(custom_event_id)
+    # however since the we are still running a command in the foreground we can not simply execute
+    # something fusion related from the custom event handler.
+    # Instead we need to call the commad.doExecute(False) method and execute the work tbd from the
+    # active commands execute handler.
+    # To achieve arbitrary execution we utilize a global execution queue.
+
+    # Note that sometimes it might also work to directly something from the custom event handler
+    # or call the command.doExecute(False) method directly from the thread.
+    # However the most reliable method is the one described above.
+
     # thread must be global so it can be killed in the destroy handler
     global periodic_thread
-    periodic_thread = faf.utils.PeriodicExecuter(2, thread_execute)
-    periodic_thread.start()
 
-    # cusotm events need to be created here. If they get created later they wont work.
-    faf.utils.create_custom_event("CommandActionTriggerEventId", on_custom_event)
+    # in the thread we fire an custom event which fille the execution queue and triggers the command.execute(False) method
+    # this can be realized in two method:
+    # 1.) The classsic one:
+    # 1.1) Create a cusotm event here. If they get created later they wont work. The event handler triggers the command.execute()
+    faf.utils.create_custom_event(
+        "CommandActionTriggerEventId", lambda event_args: trigger_command_queue()
+    )
+    # 1.2) fire the custom event from the thread (see thread execute)
+    periodic_thread = faf.utils.PeriodicExecuter(
+        2,
+        lambda: adsk.core.Application.get().fireCustomEvent(
+            "CommandActionTriggerEventId"
+        ),
+    )
+
+    # 2.) Use generic utility for custo events
+    # periodic_thread = faf.utils.PeriodicExecuter(
+    #     2,
+    #     lambda: faf.utils.execute_as_event(
+    #         trigger_command_queue, event_id="CommandActionTriggerEventId"
+    #     ),
+    # )
+
+    periodic_thread.start()
 
     # if we are in the on created handler we can not use the command.doExecute workaround since
     # the command hasnt been created yet. Also using a custom event as intermediate step wont help.
     # Therefore the initial action must be called directly from here
-    faf.utils.create_cube((0,0,10),10)
+    adsk.core.Application.get().activeProduct.rootComponent.bRepBodies.add(
+        faf.utils.create_cube((0, 0, 10), 10)
+    )
 
 
-# def on_input_changed(event_args: adsk.core.InputChangedEventArgs):
-#     if event_args.input.id == InputIds.Button1.value:
-#         # no effect at all
-#         # vox.DirectCube(ao.rootComponent, (0, 0, 0), 1, name="input changed")
-#         execution_queue.put(
-#             lambda: vox.DirectCube(ao.rootComponent, (0, 0, 0), 1, name="input changed")
-#         )
-#         adsk.core.Command.cast(event_args.firingEvent.sender).doExecute(False)
+def on_input_changed(event_args: adsk.core.InputChangedEventArgs):
+    # in the input changed handler we do NOT need to use the custom_event -> execution_queue mechanism.
+
+    global j
+
+    if event_args.input.id == InputIds.Button1.value:
+        adsk.core.Application.get().activeProduct.rootComponent.bRepBodies.add(
+            faf.utils.create_cube((0, j * 10, 10), 10)
+        )
+        j = j + 1
 
 
 def on_preview(event_args: adsk.core.CommandEventArgs):
@@ -92,9 +118,7 @@ def on_execute(event_args: adsk.core.CommandEventArgs):
 
 
 def on_destroy(event_args: adsk.core.CommandEventArgs):
-    periodic_thread.kill()
-
-
+    periodic_thread.pause()
 
 
 ### entry point ################################################################
@@ -112,7 +136,6 @@ def run(context):
         tab = faf.Tab(workspace, id="ToolsTab")
         panel = faf.Panel(tab, id="SolidScriptsAddinsPanel")
         control = faf.Control(panel)
-        custom_event_id = str(uuid4())
         cmd = faf.AddinCommand(
             control,
             resourceFolder="lightbulb",
